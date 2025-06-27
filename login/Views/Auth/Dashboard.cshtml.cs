f68e4f59da7e6558e32b283f078e0e7977e193b8
Helaman23 <helaman.linares1@gmail.com~>
2025-06-26 20:28:49 -0400

    first

---
+++ login/Views/Auth/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace login.Views.Auth
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
